a929dd63f739b5d8947d8d62edfdaae773bf9c8d
Michael Pidde <michaelpidde@fastmail.com>
2022-04-03 14:30:20 -0500

    Remove doc comment because it's 100% useless

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿class Program {
-    /// <param name="configFile">Path to server configuration file</param>
     static void Main(FileInfo? configFile) {
         if(configFile == null || !configFile.Exists) {
             Console.WriteLine("Supply a valid path to a server configuration file.");
